a492979eb707260857487f21356cf6f281aed2c0
harkishor-mobile <harkishor@cssoftsolutions.com>
2025-11-27 12:21:16 +0530

    Update database url

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,12 @@
 // src/config/db.ts
 import { PrismaClient } from '../generated/client';
 
-const prisma = new PrismaClient(); // No options allowed here
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+}); // No options allowed here
 
 export default prisma;
